16df451eaac3f0efd63fef716ae0c6f34c4ab155
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-11-20 22:34:49 -0200

    Renaming entry screen

--- app/src/main/java/nubank/hiring/ufs/br/nubankchallenge/PurchaseDetailsActivity.kt
+++ app/src/main/java/nubank/hiring/ufs/br/nubankchallenge/PurchaseDetailsActivity.kt
@@ -1,12 +1,12 @@
 package nubank.hiring.ufs.br.nubankchallenge
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class BillingDetailsActivity : AppCompatActivity() {
+class PurchaseDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_billing_details)
+        setContentView(R.layout.activity_purchase_details)
     }
 }
