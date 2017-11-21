37dd57526352803aef50d3433cde7956f1aba54a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-11-21 16:24:08 -0200

    Added networking issues handler, with tests

---
+++ core/src/main/java/br/ufs/nubankchallenge/core/infrastructure/NetworkingErrorHandler.kt
@@ -0,0 +1,58 @@
+package br.ufs.nubankchallenge.core.infrastructure
+
+import br.ufs.nubankchallenge.core.domain.errors.NetworkingIssue.*
+import io.reactivex.Observable
+import io.reactivex.ObservableSource
+import io.reactivex.ObservableTransformer
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+/**
+ *
+ * Created by @ubiratanfsoares
+ *
+ */
+
+class NetworkingErrorHandler : ObservableTransformer<Any, Any> {
+
+    override fun apply(upstream: Observable<Any>): ObservableSource<Any> {
+        return upstream.onErrorResumeNext(this::handleIfNetworkingError)
+    }
+
+    private fun handleIfNetworkingError(throwable: Throwable): Observable<Any> {
+        if (isNetworkingError(throwable)) return asNetworkingError(throwable)
+        return Observable.error(throwable)
+    }
+
+    private fun asNetworkingError(throwable: Throwable): Observable<Any> {
+        return Observable.error(mapToDomainError(throwable))
+    }
+
+    private fun mapToDomainError(throwable: Throwable): Throwable {
+
+        if (isConnectionTimeout(throwable)) return OperationTimeout
+        if (noInternetAvailable(throwable)) return InternetUnreachable
+        return ConnectionSpike
+    }
+
+    private fun isNetworkingError(throwable: Throwable): Boolean {
+        return isConnectionTimeout(throwable) ||
+                noInternetAvailable(throwable) ||
+                isRequestCanceled(throwable)
+    }
+
+    private fun isRequestCanceled(throwable: Throwable): Boolean {
+        return throwable is IOException
+                && throwable.message?.contentEquals("Canceled") ?: false
+    }
+
+    private fun noInternetAvailable(throwable: Throwable): Boolean {
+        return throwable is UnknownHostException
+    }
+
+    private fun isConnectionTimeout(throwable: Throwable): Boolean {
+        return throwable is SocketTimeoutException
+    }
+
+}
---
+++ core/src/test/java/br/ufs/nubankchallenge/core/tests/infrastructure/NetworkingErrorHandlerTests.kt
@@ -0,0 +1,53 @@
+package br.ufs.nubankchallenge.core.tests.infrastructure
+
+import br.ufs.nubankchallenge.core.domain.errors.NetworkingIssue
+import br.ufs.nubankchallenge.core.domain.errors.NetworkingIssue.*
+import br.ufs.nubankchallenge.core.infrastructure.NetworkingErrorHandler
+import io.reactivex.Observable
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+/**
+ *
+ * Created by @ubiratanfsoares
+ *
+ */
+class NetworkingErrorHandlerTests {
+
+    lateinit var handler: NetworkingErrorHandler
+
+    @Before fun `before each test`() {
+        handler = NetworkingErrorHandler()
+    }
+
+    @Test fun `should handle internet unavailable`() {
+        val noInternet = Observable.error<Any>(UnknownHostException("No Internet"))
+        noInternet.compose(handler)
+                .test()
+                .assertError(InternetUnreachable::class.java)
+    }
+
+    @Test fun `should handle connection errors`() {
+        val brokenConnection = Observable.error<Any>(IOException("Canceled"))
+        brokenConnection.compose(handler)
+                .test()
+                .assertError(ConnectionSpike::class.java)
+    }
+
+    @Test fun `should handle timeouts`() {
+        val timeout = Observable.error<Any>(SocketTimeoutException())
+        timeout.compose(handler)
+                .test()
+                .assertError(OperationTimeout::class.java)
+    }
+
+    @Test fun `should not handle other errors`() {
+        val otherError = Observable.error<Any>(IllegalStateException("Broken json"))
+        otherError.compose(handler)
+                .test()
+                .assertError({ throwable -> throwable !is NetworkingIssue })
+    }
+}
